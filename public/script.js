0ebd27aed5b70ece5df8c97621a665794fa99a5b
Vcode26 <Vcode26@outlook.com>
2026-02-24 15:11:16 -0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,29 @@
+const API = "https://YOUR_WORKER_URL";
+
+async function signup() {
+  const username = document.getElementById("signup-username").value;
+  const password = document.getElementById("signup-password").value;
+
+  const res = await fetch(`${API}/api/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  alert(JSON.stringify(data));
+}
+
+async function login() {
+  const username = document.getElementById("login-username").value;
+  const password = document.getElementById("login-password").value;
+
+  const res = await fetch(`${API}/api/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  alert(JSON.stringify(data));
+}
